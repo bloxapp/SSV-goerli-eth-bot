b39e5b78894bfef8eef2d1658020440ab90840d0
guy muroch <guym@blox.io>
2022-08-15 16:52:23 +0300

    adding new secret

--- src/faucet.js
+++ src/faucet.js
@@ -6,43 +6,51 @@ const jsonInterface = require('../src/abi.json');
 const { SSV_INFURA_HTTPS_ENDPOINT, SSV_EXPLORER_URL, SINGER_PRIVATE_KEY } = process.env;
 const web3 = new Web3(new Web3.providers.HttpProvider(SSV_INFURA_HTTPS_ENDPOINT));
 
-const ssvContract = '0x6471F70b932390f527c6403773D082A0Db8e8A9F';
 const contract = new Contract(jsonInterface, ssvContract);
 const faucetApiUrl = `${SSV_EXPLORER_URL}/api/ssv_faucet/`;
-const signerOwnerAddress = '0x67Ce5c69260bd819B4e0AD13f4b873074D479811';
+const ssvContract = '0x6471F70b932390f527c6403773D082A0Db8e8A9F';
+const signerOwnerAddress = '0x3f158913A59d5E9f81FA86faF9E9165E531E4A44';
 
 const getTransactions = async () => {
     console.log('Start fetching initiated transactions from Explorer Center')
-    let response = (await axios.get(faucetApiUrl + '?status=initiated')).data;
-    console.log(`Fetched ${+response?.length}`)
-    if(+response?.length === 0) console.log(`No transaction to execute... starting over in 2 seconds`)
-    for (let index = 0; index < response.length; index++) {
-        const userTransaction = response[index];
-        const nonce = '0x' + (await web3.eth.getTransactionCount(signerOwnerAddress)).toString(16);
-        const data = contract.methods.transfer(userTransaction.owner_address, web3.utils.toWei('10')).encodeABI();
-        const gasPrice = 990000000000;
-        const transaction = {
-            data,
-            nonce,
-            chainID: 5,
-            gas: 1000000,
-            gasPrice: gasPrice,
-            to: ssvContract,
-            from: signerOwnerAddress,
-            value: web3.utils.numberToHex(web3.utils.toWei('0', 'ether')),
+    try {
+        let response = (await axios.get(faucetApiUrl + '?status=initiated')).data;
+        console.log(`Fetched ${+response?.length}`)
+        if(+response?.length === 0) console.log(`No transaction to execute... starting over in 2 seconds`)
+        for (let index = 0; index < response.length; index++) {
+            const userTransaction = response[index];
+            const nonce = '0x' + (await web3.eth.getTransactionCount(signerOwnerAddress)).toString(16);
+            const data = contract.methods.transfer(userTransaction.owner_address, web3.utils.toWei('10')).encodeABI();
+            const gasPrice = 990000000000;
+            const transaction = {
+                data,
+                nonce,
+                chainID: 5,
+                gas: 1000000,
+                gasPrice: gasPrice,
+                to: ssvContract,
+                from: signerOwnerAddress,
+                value: web3.utils.numberToHex(web3.utils.toWei('0', 'ether')),
+            }
+            const signedTx = await web3.eth.accounts.signTransaction(transaction, SINGER_PRIVATE_KEY);
+            console.log(`Sending transaction for ${userTransaction.owner_address}`)
+            await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+                .on('transactionHash', (txHash) => {
+                    updateExplorerTransaction(userTransaction.id, userTransaction.owner_address, txHash, 'pending')
+                })
+                .on('receipt', (receipt) => {
+                    console.log(`Transaction for ${userTransaction.owner_address} finished with success`)
+                    updateExplorerTransaction(userTransaction.id, userTransaction.owner_address, undefined, 'success')
+                })
+                .on('error', (error) => {
+                    console.log(`Transaction for ${userTransaction.owner_address} finished with failure ${error.message}`)
+                    updateExplorerTransaction(userTransaction.id, userTransaction.owner_address, undefined, 'initiated')
+                })
         }
-        const signedTx = await web3.eth.accounts.signTransaction(transaction, SINGER_PRIVATE_KEY);
-        console.log(`Sending transaction for ${userTransaction.owner_address}`)
-        await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-            .on('transactionHash', (txHash) => {
-                updateExplorerTransaction(userTransaction.id, userTransaction.owner_address, txHash, 'pending')
-            })
-            .on('receipt', (receipt) => {
-                console.log(`Transaction for ${userTransaction.owner_address} finished with success`)
-                updateExplorerTransaction(userTransaction.id, userTransaction.owner_address, undefined, 'success')
-            })
+        setTimeout(getTransactions, 2000);
+    } catch (e) {
+     console.log(`[FAUCET][ERROR]: ${e.message}`);
     }
-    setTimeout(getTransactions, 2000);
 }
 
 const updateExplorerTransaction = async (transactionId, ownerAddress, txHash, status) => {
