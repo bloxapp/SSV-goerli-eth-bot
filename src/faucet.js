bf632abc0002f3306a76caac873366d67b3bab52
guy muroch <guym@blox.io>
2022-08-22 11:18:31 +0300

    adding support for production

--- src/faucet.js
+++ src/faucet.js
@@ -2,37 +2,35 @@ const Web3 = require("web3");
 require('dotenv').config();
 const axios = require("axios");
 const jsonInterface = require('../src/abi.json');
-const { SSV_INFURA_HTTPS_ENDPOINT, SSV_EXPLORER_URL, SINGER_PRIVATE_KEY } = process.env;
+const { SSV_INFURA_HTTPS_ENDPOINT, SSV_EXPLORER_URL, SINGER_PRIVATE_KEY, SIGNER_OWNER_ADDRESS, SSV_CONTRACT_ADDRESS } = process.env;
 const web3 = new Web3(new Web3.providers.HttpProvider(SSV_INFURA_HTTPS_ENDPOINT));
 
 const faucetApiUrl = `${SSV_EXPLORER_URL}/api/ssv_faucet/`;
 const faucetConfigApiUrl = `${SSV_EXPLORER_URL}/api/ssv_faucet_config/`;
-const ssvContract = '0x6471F70b932390f527c6403773D082A0Db8e8A9F';
-const contract = new web3.eth.Contract(jsonInterface, ssvContract);
-const signerOwnerAddress = '0x3f158913A59d5E9f81FA86faF9E9165E531E4A44';
+const contract = new web3.eth.Contract(jsonInterface, SSV_CONTRACT_ADDRESS);
 
 const getTransactions = async () => {
     console.log('Start fetching initiated transactions from Explorer Center')
     try {
         const faucetConfig = await getFaucetConfig();
-        const faucetBalance = await getBalance(contract, signerOwnerAddress);
+        const faucetBalance = await getBalance(contract, SIGNER_OWNER_ADDRESS);
         let response = (await axios.get(faucetApiUrl + '?status=initiated')).data;
         const transactionsCapacity = Math.floor(faucetBalance / faucetConfig?.amount_to_transfer) - +response?.length;
         if(faucetConfig.transactions_capacity !== transactionsCapacity) await updateFaucetConfig(faucetConfig, transactionsCapacity)
         if(+response?.length === 0) console.log(`No transaction to execute... starting over in 2 seconds`)
         for (let index = 0; index < response.length; index++) {
             const userTransaction = response[index];
-            const nonce = '0x' + (await web3.eth.getTransactionCount(signerOwnerAddress)).toString(16);
+            const nonce = '0x' + (await web3.eth.getTransactionCount(SIGNER_OWNER_ADDRESS)).toString(16);
             const data = contract.methods.transfer(userTransaction.owner_address, web3.utils.toWei('10')).encodeABI();
             const gasPrice = await getGasPrice();
             const transaction = {
                 data,
                 nonce,
                 chainID: 5,
-                to: ssvContract,
                 gasLimit: 1000000,
                 gasPrice: gasPrice,
-                from: signerOwnerAddress,
+                to: SSV_CONTRACT_ADDRESS,
+                from: SIGNER_OWNER_ADDRESS,
                 value: web3.utils.numberToHex(web3.utils.toWei('0', 'ether')),
             }
             const signedTx = await web3.eth.accounts.signTransaction(transaction, SINGER_PRIVATE_KEY);
